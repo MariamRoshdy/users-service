1ea0c7f27423abf13d304778781d2e007272118f
MariamRoshdy <mariemroshdy2016@gmail.com>
2025-10-04 00:35:07 +0300

    Add JWT class

---
+++ Controllers/Helpers/JWT.cs
@@ -0,0 +1,8 @@
+public class JWT
+{
+     public string Secret { get; set; } = string.Empty;
+    public string Issuer { get; set; } = string.Empty;
+    public string Audience { get; set; } = string.Empty;
+    public int AccessTokenExpirationMinutes { get; set; }
+    public int RefreshTokenExpirationDays { get; set; }
+}
--- obj/Debug/net8.0/UsersService.AssemblyInfo.cs
+++ obj/Debug/net8.0/UsersService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UsersService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3e82129e0428484c4f011c3f1f51196af056f9c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0c9efe823dd14773f201a7b4822d1aaacc8e41d")]
 [assembly: System.Reflection.AssemblyProductAttribute("UsersService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UsersService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
